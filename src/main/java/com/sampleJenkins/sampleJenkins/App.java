1cff590a8eaf12a4cd6e1ca58c626a834230fa63
githubsneha18 <sneharimipriyanshu@gmail.com>
2023-09-29 00:01:02 +0530

    initial commit

---
+++ src/main/java/com/sampleJenkins/sampleJenkins/App.java
@@ -0,0 +1,11 @@
+package com.sampleJenkins.sampleJenkins;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is sample Maven Project..." );
+    }
+}
